606ad3ff5f1e007d5b8870da83f37a5b8a985f07
takuya.nagai <nagataku021@outlook.jp>
2020-03-07 12:23:37 +0000

    users_new routes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  before_action :require_user_logged_in, except: :new
+  before_action :require_user_logged_in, only: [:index, :show, :edit, :update, :destroy]
   before_action :admin_user, only: :destroy
   
   def index
