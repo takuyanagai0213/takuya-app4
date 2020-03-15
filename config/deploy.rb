5d2c1a367665fd36225501db801f93d2795cbdb9
TakuyaNagai <nagataku021@outlook.jp>
2020-03-15 13:28:15 +0900

    デプロイにcapistranoを導入

--- config/deploy.rb
+++ config/deploy.rb
@@ -37,3 +37,45 @@ set :repo_url, "git@github.com:takuyanagai0213/fishingshares.git"
 
 # Uncomment the following to require manually verifying the host key before first deploy.
 # set :ssh_options, verify_host_key: :secure
+
+# rubyのバージョン
+set :rbenv_ruby, '2.5.3'
+
+#出力するログのレベル。
+set :log_level, :debug
+
+namespace :deploy do
+  desc 'Restart application'
+  task :restart do
+    invoke 'unicorn:restart'
+  end
+
+  desc 'Create database'
+  task :db_create do
+    on roles(:db) do |host|
+      with rails_env: fetch(:rails_env) do
+        within current_path do
+          execute :bundle, :exec, :rake, 'db:create'
+        end
+      end
+    end
+  end
+
+  desc 'Run seed'
+  task :seed do
+    on roles(:app) do
+      with rails_env: fetch(:rails_env) do
+        within current_path do
+          execute :bundle, :exec, :rake, 'db:seed'
+        end
+      end
+    end
+  end
+
+  after :publishing, :restart
+
+  after :restart, :clear_cache do
+    on roles(:web), in: :groups, limit: 3, wait: 10 do
+    end
+  end
+end
