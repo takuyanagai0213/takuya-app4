bcf5ffd441a56344ad4f593bc43b7fa67f4229f1
TakuyaNagai <nagataku021@outlook.jp>
2020-03-15 15:29:30 +0900

    デプロイにcapistranoを導入

--- config/deploy.rb
+++ config/deploy.rb
@@ -51,16 +51,16 @@ namespace :deploy do
     invoke 'unicorn:restart'
   end
 
-  desc 'Create database'
-  task :db_create do
-    on roles(:db) do |host|
-      with rails_env: fetch(:rails_env) do
-        within current_path do
-          execute :bundle, :exec, :rake, 'db:create'
-        end
-      end
-    end
-  end
+  # desc 'Create database'
+  # task :db_create do
+  #   on roles(:db) do |host|
+  #     with rails_env: fetch(:rails_env) do
+  #       within current_path do
+  #         execute :bundle, :exec, :rake, 'db:create'
+  #       end
+  #     end
+  #   end
+  # end
 
   desc 'Run seed'
   task :seed do
