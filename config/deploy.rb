a69531030af8a796650ba7588a8939c42138d32a
TakuyaNagai <nagataku021@outlook.jp>
2020-03-20 23:20:06 +0900

    capistrano

--- config/deploy.rb
+++ config/deploy.rb
@@ -68,16 +68,16 @@ namespace :deploy do
   #   end
   # end
 
-  desc 'Run seed'
-  task :seed do
-    on roles(:app) do
-      with rails_env: fetch(:rails_env) do
-        within current_path do
-          execute :bundle, :exec, :rake, 'db:seed'
-        end
-      end
-    end
-  end
+  # desc 'Run seed'
+  # task :seed do
+  #   on roles(:app) do
+  #     with rails_env: fetch(:rails_env) do
+  #       within current_path do
+  #         execute :bundle, :exec, :rake, 'db:seed'
+  #       end
+  #     end
+  #   end
+  # end
 
   after :publishing, :restart
 
