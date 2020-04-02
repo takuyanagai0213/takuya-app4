a8c80df7c9e5fd349e957101e50fc357c9281b22
TakuyaNagai <nagataku021@outlook.jp>
2020-04-02 20:29:27 +0900

    Rspecを変更

---
+++ spec/models/post_spec.rb
@@ -0,0 +1,50 @@
+require 'rails_helper'
+
+RSpec.describe Post, type: :model do
+    # ユーザー単位では重複したpostを許可しないこと
+    it "does not allow duplicate post names per user" do
+      user = User.create(
+        name: "Joe",
+        email: "joetester@example.com",
+        password: "dottle-nouveau-pavilion-tights-furze",
+      )
+  
+      user.posts.create(
+        name: "Test post",
+      )
+  
+      new_post = user.posts.build(
+        name: "Test post",
+      )
+  
+      new_post.valid?
+      expect(new_post.errors[:name]).to include("has already been taken")
+    end
+  
+    # 二人のユーザーが同じ名前を使うことは許可すること
+    it "allows two users to share a project name" do
+      user = User.create(
+        name: "Joe",
+        email: "joetester@example.com",
+        password: "dottle-nouveau-pavilion-tights-furze",
+      )
+  
+      user.posts.create(
+        name: "Test post",
+      )
+  
+      other_user = User.create(
+        name: "Jane",
+        email: "joetester@example.com",
+        password: "dottle-nouveau-pavilion-tights-furze",
+      )
+  
+      other_post = other_user.posts.build(
+        name: "Test post",
+      )
+      
+  
+      expect(other_post).to_not be_valid
+    end
+
+end
