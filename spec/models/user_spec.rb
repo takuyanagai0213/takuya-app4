cc40fce3adc320f8c6c2cfb6206f39bebd4c6e81
TakuyaNagai <nagataku021@outlook.jp>
2020-04-02 19:55:21 +0900

    Rspecを変更

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,55 +1,47 @@
-# # frozen_string_literal: true
+require 'rails_helper'
 
-# require 'rails_helper'
+RSpec.describe User, type: :model do
+  # 姓、名、メール、パスワードがあれば有効な状態であること
+  it "is valid with name, email, and password" do
+    user = User.new(
+      name: "Aaron",
+      email: "abc@example.com",
+      password: "dottle-nouveau-pavilion-tights-furze",
+    )
+    expect(user).to be_valid
+  end
 
-# RSpec.describe User, type: :model do
-#   it 'is valid with email and password' do
-#     user = FactoryBot.build(:user)
-#     expect(user).to be_valid
-#   end
+  # 名前がなければ無効な状態であること
+  it "is invalid without name" do
+    user = User.new(name: nil)
+    user.valid?
+    expect(user.errors[:name]).to include("can't be blank")
+  end
 
-#   it 'is invalid without email' do
-#     user = FactoryBot.build(:user, email: nil)
-#     user.valid?
-#     expect(user.errors[:email]).to include('を入力してください')
-#   end
 
-#   it 'is invalid without password' do
-#     user = FactoryBot.build(:user, password: nil)
-#     user.valid?
-#     expect(user.errors[:password]).to include('を入力してください')
-#   end
+  # メールアドレスがなければ無効な状態であること
+  it "is invalid without an email address" do
+    user = User.new(email: nil)
+    user.valid?
+    expect(user.errors[:email]).to include("can't be blank")
+  end
 
-#   it 'is invalid if password is less 6 characters' do
-#     user = FactoryBot.build(:user, password: 'test')
-#     user.valid?
-#     expect(user.errors[:password]).to include('は6文字以上で入力してください')
-#   end
+  # 重複したメールアドレスなら無効な状態であること
+  it "is invalid with a duplicate email address" do
+    User.create(
+      first_name: "Joe",
+      last_name: "Tester",
+      email: "tester@example.com",
+      password: "dottle-nouveau-pavilion-tights-furze",
+    )
 
-#   it 'is invalid if email is already taken' do
-#     FactoryBot.create(:user, email: 'duplicate@email.com')
-#     user = FactoryBot.build(:user, email: 'duplicate@email.com')
-#     user.valid?
-#     expect(user.errors[:email]).to include('はすでに存在します')
-#   end
-
-#   it 'can have many posts' do
-#     user = FactoryBot.create(:user, :with_posts)
-#     expect(user.posts.length).to eq 5
-#   end
-
-#   it 'can have many comments' do
-#     user = FactoryBot.create(:user, :with_comments)
-#     expect(user.comments.length).to eq 5
-#   end
-
-#   it 'can have many likes' do
-#     user = FactoryBot.create(:user, :with_likes)
-#     expect(user.likes.length).to eq 5
-#   end
-
-#   it 'can have many stockitems' do
-#     user = FactoryBot.create(:user, :with_stockitems)
-#     expect(user.stockitems.length).to eq 5
-#   end
-# end
+    user = User.new(
+      first_name: "Jane",
+      last_name: "Tester",
+      email: "tester@example.com",
+      password: "dottle-nouveau-pavilion-tights-furze",
+    )
+    user.valid?
+    expect(user.errors[:email]).to include("has already been taken")
+  end
+end
--- terraform/VPC/main.tf
+++ terraform/VPC/main.tf
@@ -5,72 +5,72 @@ provider "aws" {
 }
 
 # VPC作成
-resource "aws_vpc" "aws-tf-vpc" {
-  cidr_block           = "10.1.0.0/16"
+resource "aws_vpc" "VPC_for_FishingShares" {
+  cidr_block           = "10.0.0.0/16"
   instance_tenancy     = "default"
   enable_dns_support   = "true"
   enable_dns_hostnames = "true"
   tags = {
-    Name = "aws-tf-vpc"
+    Name = "VPC_for_FishingShares"
   }
 }
 
 # サブネット２つ作成(publicとprivate)
-resource "aws_subnet" "aws-tf-public-subnet-1a" {
-  vpc_id            = aws_vpc.aws-tf-vpc.id
-  cidr_block        = "10.1.1.0/24"
+resource "aws_subnet" "FishingShares-subnet" {
+  vpc_id            = aws_vpc.VPC_for_FishingShares.id
+  cidr_block        = "10.0.1.0/24"
   availability_zone = "ap-northeast-1a"
   tags = {
-    Name = "aws-tf-public-subnet-1a"
+    Name = "FishingShares-subnet"
+  }
+}
+resource "aws_subnet" "FishingShares-subnet-DB" {
+  vpc_id            = aws_vpc.VPC_for_FishingShares.id
+  cidr_block        = "10.0.2.0/24"
+  availability_zone = "ap-northeast-1a"
+  tags = {
+    Name = "FishingShares-subnet-DB"
   }
 }
 
-resource "aws_subnet" "aws-tf-private-subnet-1a" {
-  vpc_id            = aws_vpc.aws-tf-vpc.id
-  cidr_block        = "10.1.20.0/24"
-  availability_zone = "ap-northeast-1a"
-  tags = {
-    Name = "aws-tf-private-subnet-1a"
-  }
-}
 
 # インターネットゲートウェイの作成
-resource "aws_internet_gateway" "aws-tf-igw" {
-  vpc_id = aws_vpc.aws-tf-vpc.id
+resource "aws_internet_gateway" "Gateway_for_FishigShares" {
+  vpc_id = aws_vpc.VPC_for_FishingShares.id
   tags = {
-    Name = "aws-tf-igw"
+    Name = "Gateway_for_FishigShares"
   }
 }
 
 # ルートテーブルの作成
-resource "aws_route_table" "aws-tf-public-route" {
-  vpc_id = aws_vpc.aws-tf-vpc.id
+resource "aws_route_table" "Table_for_FishingShares" {
+  vpc_id = aws_vpc.VPC_for_FishingShares.id
   route {
     cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.aws-tf-igw.id
+    gateway_id = aws_internet_gateway.Gateway_for_FishigShares.id
   }
   tags = {
-    Name = "aws-tf-public-route"
+    Name = "Table_for_FishingShares"
   }
 }
 
 # サブネットの関連付けでルートテーブルをパブリックサブネットに紐付け
 resource "aws_route_table_association" "aws-tf-public-subnet-association" {
-  subnet_id      = aws_subnet.aws-tf-public-subnet-1a.id
-  route_table_id = aws_route_table.aws-tf-public-route.id
+  subnet_id      = aws_subnet.FishingShares-subnet.id
+  route_table_id = aws_route_table.Table_for_FishingShares.id
 }
 
 # EC2作成(public側)
-resource "aws_instance" "aws-tf-web" {
+resource "aws_instance" "fishingshares-webserver" {
   ami                     = "ami-011facbea5ec0363b"
   instance_type           = "t2.micro"
   disable_api_termination = false
   key_name = aws_key_pair.auth.key_name
-  vpc_security_group_ids  = [aws_security_group.aws-tf-web.id]
-  subnet_id               = aws_subnet.aws-tf-public-subnet-1a.id
+  vpc_security_group_ids  = [aws_security_group.fishingshares-webserver.id]
+  subnet_id               = aws_subnet.FishingShares-subnet.id
 
   tags = {
-    Name = "aws-tf-web"
+    Name = "fishingshares-webserver"
   }
 }
 variable "public_key_path" {}
@@ -81,12 +81,12 @@ resource "aws_key_pair" "auth" {
 }
 
 # Security Group
-resource "aws_security_group" "aws-tf-web" {
+resource "aws_security_group" "fishingshares-webserver" {
   name        = "aws-tf-web"
   description = "aws-tf-web_sg"
-  vpc_id      = aws_vpc.aws-tf-vpc.id
+  vpc_id      = aws_vpc.VPC_for_FishingShares.id
   tags = {
-    Name = "aws-tf-web"
+    Name = "fishingshares-webserver"
   }
 }
 
@@ -101,7 +101,7 @@ resource "aws_security_group_rule" "inbound_http" {
   ]
 
   # ここでweb_serverセキュリティグループに紐付け
-  security_group_id = aws_security_group.aws-tf-web.id
+  security_group_id = aws_security_group.fishingshares-webserver.id
 }
 
 # 22番ポート許可のインバウンドルール
@@ -115,7 +115,7 @@ resource "aws_security_group_rule" "inbound_ssh" {
   ]
 
   # ここでweb_serverセキュリティグループに紐付け
-  security_group_id = aws_security_group.aws-tf-web.id
+  security_group_id = aws_security_group.fishingshares-webserver.id
 }
 
 # アウトバウンドルール
@@ -129,70 +129,14 @@ resource "aws_security_group_rule" "outbound_all" {
   ]
 
   # ここでweb_serverセキュリティグループに紐付け
-  security_group_id = aws_security_group.aws-tf-web.id
+  security_group_id = aws_security_group.fishingshares-webserver.id
 }
 
 # ElasticIP
 resource "aws_eip" "aws-tf-eip" {
-  instance = aws_instance.aws-tf-web.id
+  instance = aws_instance.fishingshares-webserver.id
   vpc      = true
 }
 output "example-public-ip" {
     value = aws_eip.aws-tf-eip.public_ip
-}
-
-# RDS用のサブネットを作成
-resource "aws_subnet" "aws-tf-private-subnet-1c" {
-  vpc_id = aws_vpc.aws-tf-vpc.id
-  cidr_block = "10.1.3.0/24"
-  availability_zone = "ap-northeast-1c"
-  tags = {
-    Name = "aws-tf-private-subnet-1c"
-  }
-}
-# 使用する２つを指定します。
-resource "aws_db_subnet_group" "rdb-tf-db" {
-    name        = "rdb-tf-db-subnet"
-    description = "It is a DB subnet group on tf_vpc."
-    subnet_ids  = [aws_subnet.aws-tf-private-subnet-1a.id,aws_subnet.aws-tf-private-subnet-1c.id]
-    tags = {
-        Name = "rdb-tf-db"
-    }
-}
-
-# Security Group
-resource "aws_security_group" "aws-tf-db" {
-    name        = "aws-tf-db"
-    description = "aws-tf-db-sg"
-    vpc_id      = aws_vpc.aws-tf-vpc.id
-    tags = {
-      Name = "aws-tf-db"
-    }
-}
-
-resource "aws_security_group_rule" "db" {
-    type                     = "ingress"
-    from_port                = 5432
-    to_port                  = 5432
-    protocol                 = "tcp"
-    source_security_group_id = aws_security_group.aws-tf-web.id
-    security_group_id        = aws_security_group.aws-tf-db.id
-}
-
-
-variable "aws-td-db-username" {}
-variable "aws-td-db-password" {}
-
-resource "aws_db_instance" "aws-td-db" {
-  identifier              = "aws-td-db"
-  allocated_storage       = 20
-  name = "db11"
-  engine                  = "MySQL"
-  engine_version          = "5.7.22"
-  instance_class          = "db.t2.micro"
-  storage_type            = "gp2"
-  username                = var.aws-td-db-username
-  password                = var.aws-td-db-password
-  vpc_security_group_ids  = [aws_security_group.aws-tf-db.id]
-  db_subnet_group_name    = aws_db_subnet_group.rdb-tf-db.name
 }
