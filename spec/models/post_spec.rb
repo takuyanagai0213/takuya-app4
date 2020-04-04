e459338efe91392f2a5b5697ab9677f667edabc1
TakuyaNagai <nagataku021@outlook.jp>
2020-04-04 18:15:53 +0900

    post_spec.rbを修正

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,24 +1,26 @@
 require 'rails_helper'
 
 RSpec.describe Post, type: :model do
-    it 'is invalid with title' do
-      user = FactoryBot.create(:user)
 
-      post = user.posts.build(
-         title: nil,
-         )
+    # タイトルがなければ無効な状態であること
+    it 'is invalid with title' do
+      post = FactoryBot.build(:user, title: nil,)
       post.valid?
       expect(post.errors[:title]).to include("を入力してください")
     end
 
+    # 釣行レポートがなければ無効な状態であること
     it 'is invalid with detail' do
-      user = FactoryBot.create(:user)
-
-      post = user.posts.build(
-         detail: nil,
-         )
+      post = FactoryBot.build(:user, detail: nil,)
       post.valid?
       expect(post.errors[:detail]).to include("を入力してください")
+    end
+    
+    # タイトル画像がなければ無効な状態であること
+    it 'is invalid with picture' do
+      post = FactoryBot.build(:user, picture: nil,)
+      post.valid?
+      expect(post.errors[:picture]).to include("を入力してください")
     end
   end
 
