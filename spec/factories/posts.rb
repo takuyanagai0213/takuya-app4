c17b8905c322086c2c1c2c455deec3d589af9d57
TakuyaNagai <nagataku021@outlook.jp>
2020-04-05 23:22:08 +0900

    Rspecにテストを追加

--- spec/factories/posts.rb
+++ spec/factories/posts.rb
@@ -2,6 +2,10 @@ FactoryBot.define do
   factory :post do
     sequence(:title) { |n| "釣果タイトルサンプル#{n}" }
     detail { "釣行レポートサンプル" }
-    picture { true }
+    picture { Rack::Test::UploadedFile.new(File.join(Rails.root, 'spec/fixtures/test.jpg')) }
+    
+    trait :with_picture do
+      picture { File.new("#{Rails.root}/spec/fixtures/picture.jpg") }
+    end
   end
 end
