dc2dfe5a90e4ca6df16263ba000ee349c4901777
TakuyaNagai <nagataku021@outlook.jp>
2020-04-06 00:20:22 +0900

    Rspecを修正

--- spec/controllers/posts_controller_spec.rb
+++ spec/controllers/posts_controller_spec.rb
@@ -9,6 +9,6 @@ RSpec.describe PostsController, type: :controller do
 		# it "responds successfully" do
 		# 	get :show, params: { id: @post.id }
 		# 	expect(response).to have_http_status "200"
-		end
+		# end
 	end
 end
