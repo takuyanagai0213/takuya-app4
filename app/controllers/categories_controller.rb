65c8952fdbcd230f3af00bb0e2a3cf73a0174cda
TakuyaNagai <nagataku021@outlook.jp>
2020-04-10 12:49:20 +0900

    パーシャルに書き換え

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -2,6 +2,8 @@ class CategoriesController < ApplicationController
     def show 
         @categories = Category.all
         @category = Category.find(params[:id])
-        @posts = @category.posts.all
+        @category_posts = @category.posts.all
+        @posts = Post.all.page(params[:page]).page(params[:page]).per(5)
+
     end
 end
