cc0b194a3938d1bc619c78f4355e2fd7959a6f52
TakuyaNagai <nagataku021@outlook.jp>
2020-04-19 21:37:57 +0900

    テストユーザを追加

---
+++ app/assets/javascripts/map.js
@@ -0,0 +1,6 @@
+function initMap(){
+    let map = new google.maps.Map(document.getElementById('map'), {
+    center: {lat: <%= @post.latitude %>, lng: <%= @post.longitude %> },
+    zoom: 15
+    });
+}
